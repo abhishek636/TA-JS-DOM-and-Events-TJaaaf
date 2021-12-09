fc1547518c93bb0928d589bbf161e76305461046
Abhishek Bhatt <bhattabhishek636@gmail.com>
2021-12-09 15:47:41 +0530

    block-BJaaex

--- block-BJaaex/code/script.js
+++ block-BJaaex/code/script.js
@@ -1,41 +1,43 @@
+
+
 'use strict';
 
 var cardsArray = [{
   'name': 'shell',
-  'img': 'img/blueshell.png'
+  'img': './img/blueshell.png'
 }, {
   'name': 'star',
-  'img': 'img/star.png'
+  'img': './img/star.png'
 }, {
   'name': 'bobomb',
-  'img': 'img/bobomb.png'
+  'img': './img/bobomb.png'
 }, {
   'name': 'mario',
-  'img': 'img/mario.png'
+  'img': './img/mario.png'
 }, {
   'name': 'luigi',
-  'img': 'img/luigi.png'
+  'img': './img/luigi.png'
 }, {
   'name': 'peach',
-  'img': 'img/peach.png'
+  'img': './img/peach.png'
 }, {
   'name': '1up',
-  'img': 'img/1up.png'
+  'img': './img/1up.png'
 }, {
   'name': 'mushroom',
-  'img': 'img/mushroom.png'
+  'img': './img/mushroom.png'
 }, {
   'name': 'thwomp',
-  'img': 'img/thwomp.png'
+  'img': './img/thwomp.png'
 }, {
   'name': 'bulletbill',
-  'img': 'img/bulletbill.png'
+  'img': './img/bulletbill.png'
 }, {
   'name': 'coin',
-  'img': 'img/coin.png'
+  'img': './img/coin.png'
 }, {
   'name': 'goomba',
-  'img': 'img/goomba.png'
+  'img': './img/goomba.png'
 }];
 
 var gameGrid = cardsArray.concat(cardsArray).sort(function () {
